caae59bec96263d6db65067c3c3b8a6b80af3ec2
Jeremy Muniak <Jeremy.e.Muniak@gmail.com>
2019-07-21 21:22:27 -0400

    organized my js

--- javascript/game.js
+++ javascript/game.js
@@ -2,30 +2,6 @@
 $(document).ready(function () {
     console.log("ready!");
 
-    // classes from HTML if needed
-
-    // .scoreBoardDiv {
-    // }
-    // .winningNumber {
-    // }
-    // .wins {
-    // }
-    // .loss {
-    // }
-
-    // .crystal1 {
-    // }
-    // .crystal2 {
-    // }
-    // .crystal3 {
-    // }
-    // .crystal4 {
-    // }
-    // .userTotal {
-    // }
-
-
-
     // vars and constants
     random = Math.floor(Math.random() * 100 + 19);
     console.log(random);
@@ -37,6 +13,7 @@ $(document).ready(function () {
     $(".wins").text("Wins: " + wins);
     $(".loss").text("Losses: " + losses);
 
+    // generates a random number for each crystal
     var crystalValues = {};
     crystalValues[1] = Math.floor(Math.random() * 12 + 1);
     crystalValues[2] = Math.floor(Math.random() * 12 + 1);
@@ -44,6 +21,43 @@ $(document).ready(function () {
     crystalValues[4] = Math.floor(Math.random() * 12 + 1);
     console.log(crystalValues);
 
+    // functions
+    // click funtion
+    $(".buttons div").on("click", function (event) {
+        console.log(this);
+        let _this = $(this);
+        let indexValue = _this.attr("index-value");
+        console.log(indexValue);
+        getCrystalHandler(indexValue);
+        $(".userTotal").text("Your total score is: " + userTotal);
+    });
+     // resets the game
+     function reset() {
+        random = Math.floor(Math.random() * 102 + 19);
+        console.log(random);
+        $(".winningNumber").text(random);
+        crystalValues[1] = Math.floor(Math.random() * 12 + 1);
+        crystalValues[2] = Math.floor(Math.random() * 12 + 1);
+        crystalValues[3] = Math.floor(Math.random() * 12 + 1);
+        crystalValues[4] = Math.floor(Math.random() * 12 + 1);
+        userTotal = 0;
+        $(".userTotal").text("Your total: " + userTotal);
+    }
+    // winner alert function
+    function winner() {
+        alert("You Won!!");
+        wins++;
+        $(".wins").text("Wins: " + wins);
+        reset();
+    }
+    // loser function
+    function loser() {
+        alert("You Lose!!");
+        losses++;
+        $(".loss").text("Losses: " + losses);
+        reset();
+    }
+    // getCrystalHandler adds the userTotal to whatever the value is of the key that is pressed and checks for a win/loss condition.
     function getCrystalHandler(crystalKey) {
         userTotal = userTotal + crystalValues[crystalKey];
         console.log("New userTotal " + userTotal);
@@ -58,49 +72,8 @@ $(document).ready(function () {
         }
     }
 
-    $(".buttons div").on("click", function (event) {
-        console.log(this);
-        let _this = $(this);
-        let indexValue = _this.attr("index-value");
-        console.log(indexValue);
-        getCrystalHandler(indexValue);
-        $(".userTotal").text("Your total score is: " + userTotal);
-    });
-
-    function winner() {
-        alert("You Won!!");
-        wins++;
-        $(".wins").text("Wins: " + wins);
-        reset();
-    }
-
-    function loser() {
-        alert("You Lose!!");
-        losses++;
-        $(".loss").text("Losses: " + losses);
-        reset();
-    }
-
-
-
-    // $(".crystal1").on("click", getCrystalHandler(1));
-    // $(".crystal2").on("click", getCrystalHandler(2));
-    // $(".crystal3").on("click", getCrystalHandler(3));
-    // $(".crystal4").on("click", getCrystalHandler(4));
-
-    function reset() {
-        random = Math.floor(Math.random() * 102 + 19);
-        console.log(random);
-        $(".winningNumber").text(random);
-        crystalValues[1] = Math.floor(Math.random() * 12 + 1);
-        crystalValues[2] = Math.floor(Math.random() * 12 + 1);
-        crystalValues[3] = Math.floor(Math.random() * 12 + 1);
-        crystalValues[4] = Math.floor(Math.random() * 12 + 1);
-        userTotal = 0;
-        $(".userTotal").text("Your total: " + userTotal);
-
-    }
-
-
-
 });
+// $(".crystal1").on("click", getCrystalHandler(1));
+// $(".crystal2").on("click", getCrystalHandler(2));
+// $(".crystal3").on("click", getCrystalHandler(3));
+// $(".crystal4").on("click", getCrystalHandler(4));
